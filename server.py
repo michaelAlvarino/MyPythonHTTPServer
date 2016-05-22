f1c6d2ede8e9831dee50c57ab690ffd50a50bc47
root <malvarino>
2016-05-22 17:48:00 -0400

    fix tabbing and separate out get_file

--- server.py
+++ server.py
@@ -3,33 +3,40 @@ import urllib.parse
 import os
 
 class MyRequestHandler(http.server.SimpleHTTPRequestHandler):
-	def do_GET(self):
-		self.log_message("uri: " + urllib.parse.urlparse(self.path)[2])
-		parsed = urllib.parse.urlparse(self.path)[2].split("/")
+    def do_GET(self):
+        self.log_message("uri: " + urllib.parse.urlparse(self.path)[2])
+        parsed = urllib.parse.urlparse(self.path)[2].split("/")
         # add something to check if it's an api request
-        # otherwise handle html request...
-		if '' == parsed[len(parsed) - 1]:
-			parsed[len(parsed) - 1] = "index.html"
-		if not '.' in parsed[len(parsed) - 1]:
-			parsed[len(parsed) - 1] += ".html"
-        # check if the path exists
-		p = os.getcwd() + "/".join(parsed)
-		self.log_message("full path: " + p)
-        # if it does, f = the file, send good headers
-		if os.path.isfile(p):
-			f = open(p, 'rb', 0)
-			self.send_response_only(200)
-			self.send_header('Content-Type', 'text/html')
-			self.end_headers()
-		else:
-			#file dne, send 400NotFound and 400 response
-			f = open(os.getcwd() + "/400NotFound.html", 'rb', 0)
-			self.send_response_only(400)
-			self.send_header('Content-Type', 'text/html')
-			self.end_headers()
+        if 1==1:
+            self.get_file(parsed)
 
-		# send whichever file and the http 'end of message'
-		self.wfile.write(f.read())
+    def get_file(self, parsedURI):
+    	# append index.html or .html if necessary
+        if '' == parsedURI[len(parsedURI) - 1]:
+            parsedURI[len(parsedURI) - 1] = "index.html"
+        if not '.' in parsedURI[len(parsedURI) - 1]:
+            parsedURI[len(parsedURI) - 1] += ".html"
+        # check if the path exists
+        p = os.getcwd() + "/".join(parsedURI)
+        self.log_message("full path: " + p)
+        # if it does, f = the file, send good headers
+        if os.path.isfile(p):
+            f = open(p, 'rb', 0)
+            self.send_response_only(200)
+            self.send_header('Content-Type', 'text/html')
+            self.end_headers()
+        else:
+            #file dne, send 400NotFound and 400 response
+            f = open(os.getcwd() + "/400NotFound.html", 'rb', 0)
+            self.send_response_only(400)
+            self.send_header('Content-Type', 'text/html')
+            self.end_headers()
+
+        # send whichever file and the http 'end of message'
+        self.wfile.write(f.read())
+
+    def get_api(self, parsedURI):
+        pass
 
 # server address
 server_address = ('localhost', 8000)
